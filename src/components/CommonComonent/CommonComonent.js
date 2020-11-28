9d6bea182b360cf17b22f73cfbf87cd751529267
engineer-aslam-hossain <engineer.aslamhossain@gmail.com>
2020-11-28 22:06:05 +0600

    task completed

--- src/components/CommonComonent/CommonComonent.js
+++ src/components/CommonComonent/CommonComonent.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import CancelIcon from '@material-ui/icons/Cancel';
 const CommonComonent = ({ singleData }) => {
+  const handleCros = e => {
+    e.currentTarget.remove();
+  };
   return (
     <div className='col-md-6 halfColumn' key={singleData.id}>
       <h3 className='title'>{singleData.title} </h3>
       <p className='details'>{singleData.details} </p>
       <ul>
         {singleData.item.map(itemData => (
-          <li key={itemData.id}>
+          <li key={itemData.id} onClick={e => handleCros(e)}>
             <CancelIcon
               style={{
                 marginRight: '1rem',
